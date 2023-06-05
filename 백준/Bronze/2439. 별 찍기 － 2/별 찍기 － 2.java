5b619d0f674dc1d9ba1cd221ebe5d7dcd6e2110b
tlstmddnjs <97091772+tlstmddnjs@users.noreply.github.com>
2023-06-06 01:17:08 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 324 ms, Memory: 19180 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int n=Integer.parseInt(br.readLine());
+		br.close();
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=n;j>i;j--) {
+				System.out.print(" ");
+			}
+			for(int k=0;k<i;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

6aec698ec9210cf4d2b6f1d353d301617c0cc521
seungwon9 <97091772+seungwon9@users.noreply.github.com>
2024-04-01 14:08:09 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int chicken) {
+    int service = 0;
+    while(chicken>=10){
+        service += chicken/10;
+        chicken = chicken/10 + chicken%10;
+    }
+    return service;
+}
